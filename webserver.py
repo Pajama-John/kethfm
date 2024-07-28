e1be670db886d32f5df30ce9dff9b20378fa7297
pajama-john <jntiggy@gmail.com>
2024-07-28 13:20:57 -0400

    Fixed KETH Logo

--- webserver.py
+++ webserver.py
@@ -13,7 +13,7 @@ def article1():
 #This allows you to change the image on the main Breaking News article
 @app.route('/breakingnewsimage')
 def breakingnewsimage():
-    return send_from_directory(app.static_folder, "images/ThumbnailH3SHow2")
+    return send_from_directory(app.static_folder, "images/ThumbnailH3SHow2.jpg")
 
 if __name__ == "__main__":
     app.run(port=666, debug=True)
