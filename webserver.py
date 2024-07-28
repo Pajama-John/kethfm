0236722003e0656ac1563f3487f8883ef8a78ad1
pajama-john <jntiggy@gmail.com>
2024-07-28 13:01:41 -0400

    added breaking news image

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +9,11 @@ def home():
 @app.route('/article1')
 def article1():
     return render_template("article.html")
+
+#This allows you to change the image on the main Breaking News article
+@app.route('/breakingnewsimage')
+def breakingnewsimage():
+    return send_from_directory(app.static_folder, "images/ThumbnailH3SHow2")
 
 if __name__ == "__main__":
     app.run(port=666, debug=True)
